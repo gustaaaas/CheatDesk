1045cbf0b7837407dafd674d7dbb82df860389ff
kopustaaa <gustas.cikanavicius@gmail.com>
2025-04-24 23:20:33 +0300

    Added a timeout for the beggining , since the cmd doesnt load for somereason that fast

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
 #include "roles.h"
-
+#include "cheatdesk_theme.h"
+void warmUpConsole() {
+    fflush(stdout);
+    Sleep(300);
+}
 int main(void) {
+    int doesntmatter=0;
+    warmUpConsole();
+    printf("\nPress (1) and then enter\n");
+    scanf("%s", &doesntmatter);
+    system("cls");
     int choice;
     struct Roles roles[MAX_ROLES];
 
-    int RoleCount=LoadDB(roles);
-    printf("\033[1;36m");
-    printf("====================================\n");
-    printf("       	 WELCOME TO CHEATDESK       \n");
-    printf("====================================\n");
-    printf("\033[0m");
+    int RoleCount = LoadDB(roles);
+    Intro();
     while (1) {
         PrintMenu();
         scanf("%d",&choice);
         getchar();
         if (choice == 1) {
+            system("cls");
             char *SelectedRoles = RoleSelector(RoleCount, roles);
             char command[100];
 
@@ -26,24 +33,25 @@ int main(void) {
                 if (index >= 0 && index < RoleCount) {
                     sprintf(command, "%s", roles[index].Filename);
                 } else {
-                    printf("Invalid index.\n");
+                    printf(RED "Invalid index.\n" RESET);
                     continue;
                 }
             } else {
                 sprintf(command, "%s", SelectedRoles);
             }
 
-            printf("Running: %s\n", command);
+            printf(RED"Running: %s\n \n loading...\n\n"RESET, command);
             int launch = system(command);
         }
         else if (choice == 2) {
+            system("cls");
             char* Roles=RoleCreator();
             strcpy(roles[RoleCount].Name,Roles);
             FileCreating(Roles);
             RoleCount++;
         }
         else if (choice == 3) {
-            //nesugalvojau dar
+        //nesugalvojau dar
         }
         else if (choice ==5) {
             return 0;
